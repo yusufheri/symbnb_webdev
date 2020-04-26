cb9f7ef8feed6d36b79a31ef5503e77d680cbe5b
YUSUF HERI <yusufheri64@gmail.com>
2020-04-26 08:49:56 +0100

    Mise en place Symfony-encore-bundle pour nos CSS et Javascript

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     CMEN\GoogleChartsBundle\CMENGoogleChartsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
